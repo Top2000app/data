32f6265469077e2f66057995a756fbaa12f05758
Rick Neeft <rick.neeft@outlook.com>
2024-06-16 22:48:53 +0200

    Added the code for the data generator

---
+++ src/Top2000.Data.StaticDataSiteGenerator/PublishOnlyApplication.cs
@@ -0,0 +1,29 @@
+﻿namespace Top2000.Data.StaticApiGenerator;
+
+public sealed class PublishOnlyApplication : IRunApplication
+{
+    private readonly IFileCreator fileCreator;
+
+    public PublishOnlyApplication(IFileCreator fileCreator)
+    {
+        this.fileCreator = fileCreator;
+    }
+
+    public async Task RunAsync()
+    {
+        var location = $"output";
+
+        if (Directory.Exists(location))
+        {
+            Directory.Delete(location, recursive: true);
+        }
+
+        Directory.CreateDirectory(location);
+
+        await Task.WhenAll
+        (
+            fileCreator.CreateApiFileAsync(location),
+            fileCreator.CreateDataFilesAsync(location)
+        );
+    }
+}
