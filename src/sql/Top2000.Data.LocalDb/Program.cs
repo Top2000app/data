8dbde79b7ef1023f2637defeb6621353b2eedc86
Rick Neeft <rick.neeft@outlook.com>
2024-08-18 23:55:42 +0200

    Moved code into a sql folder

---
+++ src/sql/Top2000.Data.LocalDb/Program.cs
@@ -0,0 +1,29 @@
+﻿var connectionString = @"Server=(localdb)\mssqllocaldb;Database=Top2000;";
+
+EnsureDatabase.For
+    .SqlDatabase(connectionString);
+
+var upgrader = DeployChanges.To
+    .SqlDatabase(connectionString)
+    .WithScriptEmbeddedInDataLibrary()
+    .WithTransactionPerScript()
+    .LogToConsole()
+    .Build();
+
+var result = upgrader.PerformUpgrade();
+
+var originForegroundColor = Console.BackgroundColor;
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error.ToString());
+    Console.ForegroundColor = originForegroundColor;
+    return -1;
+}
+
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!!");
+Console.ForegroundColor = originForegroundColor;
+
+return 0;
